87897b39930d01e0c96af735caa6637d640756d3
Ricky <rheredia420@gmail.com>
2021-04-20 03:37:28 -0600

    Submited finished project.

---
+++ src/Loot.js
@@ -0,0 +1,17 @@
+import Entity from "./Entity";
+
+class Loot extends Entity {
+    action (verb, world) {
+        if (verb === 'bump') {
+            world.player.add(this);
+            world.remove(this);
+        }
+        if (verb === 'drop') {
+            console.log('drop', this);
+        }
+    }
+
+
+}
+
+export default Loot;
